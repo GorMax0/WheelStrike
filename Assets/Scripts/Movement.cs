cb0eca88569e0b59bd97277ac5d09de6ec5b00b8
GorMax0 <102469814+GorMax0@users.noreply.github.com>
2022-09-23 19:53:25 +0300

    Setting Scene

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,22 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _speed;
+    [SerializeField] private float _turnSpeed;
+
+    private const float SpeedMultiplier = 2f;
+
+    private Rigidbody _rigidbody;
+    private bool _isMoving;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+   private void Update()
     {
-      if(Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0) && _isMoving == false)
         {
-            transform.Translate(Vector3.forward*Time.deltaTime);
-            transform.Rotate(Vector3.left*Time.deltaTime);
+            _isMoving = true;
+            _rigidbody.isKinematic = false;
+            _rigidbody.AddForce(Vector3.forward * _speed, ForceMode.Acceleration);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_isMoving == true)
+        {
+            transform.Rotate(-Vector3.left * Time.deltaTime * _turnSpeed);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.TryGetComponent(out Ground ground))
+        {
+
         }
     }
 }
---
+++ Assets/Scripts/StartupPower.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartupPower : MonoBehaviour
+{
+    private float _power;
+}
